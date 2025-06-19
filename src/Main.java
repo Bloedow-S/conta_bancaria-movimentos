b13183df4830a0ff0618d336d11801ec805d8ef4
Sabrina Bloedow <sabrinabloedow80@gmail.com>
2025-06-18 22:25:47 -0300

    Versão 1 aprimorada

--- src/Main.java
+++ src/Main.java
@@ -13,12 +13,12 @@ public class Main {
         String dados_inicias = String.format("""
                 \n  Seja bem vindo(a) de volta! %s!
                    
-                [%S
+                [%s
                 [Saldo: R$%d 
                 """, nome, tipo_conta, saldo);
 
         String menu = """
-                \n1. Visualizar saldo
+                1. Visualizar saldo
                 2. Transferir valor
                 3. Receber valor
                 4. Sair
@@ -29,15 +29,22 @@ public class Main {
         String data_hora_atual_ptBR = data_hora_atual.format(formato_ptBR);
         Scanner scanf = new Scanner(System.in);
 
-        //impressão
+        //imprime a "página" principal
         System.out.println(dados_inicias);
         System.out.println(menu);
         String decoracao = "-".repeat(30);
 
         while (true){
+            int escolha_usuario = 0;
 
             System.out.printf("\n%s\nDigite uma opção: ", decoracao);
-            int escolha_usuario = scanf.nextInt();
+            try {
+                escolha_usuario = scanf.nextInt();
+            } catch (Exception e) {
+                scanf.nextLine();
+                System.out.println("(!) Entrada inválida");
+                continue;
+            }
 
             if (escolha_usuario < 1 || escolha_usuario > 4) {
                 System.out.println("(!) Erro: Apenas valores de 1 à 4.");
@@ -53,15 +60,15 @@ public class Main {
                 System.out.printf("\n%s\nTransferir\n", decoracao);
                 int qde_a_transferir = 0;
 
-                //verifica se a entrada é válida
+                //padrão de código que verifica se a entrada é válida
                 while (true) {
-                    System.out.println("Quanto deseja enviar ?");
+                    System.out.println("Quanto deseja enviar? ");
                     try {
                         qde_a_transferir = scanf.nextInt();
                         break;
                     } catch (Exception e) {
-                        System.out.println("(!) Entrada inválida");
                         scanf.nextLine();
+                        System.out.println("(!) Entrada inválida");
                     }
                 }
 
@@ -69,14 +76,12 @@ public class Main {
                     System.out.println("(!) Saldo insuficiente!");
                 }
 
-
                 saldo -= qde_a_transferir;
                 String tela_transferencia_enviada = """
                         -%d
                         Saldo: R$%d  [%s]
                         """;
                 System.out.printf(tela_transferencia_enviada, qde_a_transferir, saldo, data_hora_atual_ptBR);
-
             }
 
             else if (escolha_usuario == 3) {
@@ -89,8 +94,8 @@ public class Main {
                         qde_a_receber = scanf.nextInt();
                         break;
                     } catch (Exception e) {
-                        System.out.println("(!) Entrada inválida");
                         scanf.nextLine();
+                        System.out.println("(!) Entrada inválida");
                     }
                 }
 
@@ -106,10 +111,6 @@ public class Main {
             else {
                 break;
             }
-
         }
-
-
     }
-
 }
